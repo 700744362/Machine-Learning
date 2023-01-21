1b1231389d3f1f7a449f259c8c76e5715828598c
700744362 <122392308+700744362@users.noreply.github.com>
2023-01-20 23:59:13 -0600

    Update Q1.py

--- Codes/Q1.py
+++ Codes/Q1.py
@@ -3,6 +3,8 @@ ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 ages.sort()
 print("Min age = ",min(ages))
 print("Max age = ",max(ages))
+ages.append(min(ages))
+ages.append(max(ages))
 mid=int(len(ages)/2)
 if(len(ages)%2==0):
     print("median age = ",(ages[mid]+ages[mid+1])/2)
